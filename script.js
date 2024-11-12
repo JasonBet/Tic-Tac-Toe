beff9ae9a1395b7afcdc2e6c5e52f22861b5d3ef
Jason <jasonbetsargon@gmail.com>
2024-11-11 19:00:57 -0800

    Create players array and switch active player function

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ function Cell() {
         getValue
     };
 }
+
+const GameController = (function() {
+
+    const playerOneName = "Player One";
+    const playerTwoName = "Player Two";
+    const players = [
+        {
+            pName: playerOneName,
+            playerMark: 1
+        },
+        {
+            pName: playerTwoName,
+            playerMark: 2
+        }
+    ];
+
+    // Initialize first player as active player
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        // If activePlayer is player 0, switch to player 1, else switch to player 0
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+})();
